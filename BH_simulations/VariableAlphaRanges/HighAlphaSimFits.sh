75d20456f548282246b86e30b3db3335cc63b9d8
Christopher Weiss-Lehman <weiss-lehman@Christophers-MacBook-Pro.local>
2022-01-07 10:07:20 -0700

    Cleaning up the repository before publishing a fixed version to go with the manuscript

---
+++ BH_simulations/VariableAlphaRanges/HighAlphaSimFits.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#SBATCH --account=commbayes
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1536M
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cweissle@uwyo.edu
+#SBATCH --job-name=HighAlphaFits
+
+# Max allowable is <4096M
+
+# Set the parameter combination to use and generate names of R scripts and log files
+Rscript=HighAlphaSimFits.R
+LogFile=HighAlphaSimFits.log
+
+# Change to the relevant working directory
+cd /project/commbayes/SparseInteractions/BH_sims/
+
+# Load R and MPI
+module load gcc/7.3.0 r/3.5.3 swset/2018.05 r-rstan/2.18.2-py27 openmpi/3.1.0 r-rmpi/0.6-9-r353-py27
+
+mpirun -np 1 R CMD BATCH --no-restore --no-save --quiet  $Rscript $LogFile
+
